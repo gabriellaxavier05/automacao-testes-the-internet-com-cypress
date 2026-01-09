83dc2a2854fca42987667d84af513f29a3a6c8fb
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2026-01-08 21:20:44 -0300

    Scrpit CT02 spec Checkboxes (desmarcar o último checkbox)

--- cypress/e2e/spec-checkboxes.cy.js
+++ cypress/e2e/spec-checkboxes.cy.js
@@ -1,8 +1,17 @@
 describe('Página de checkboxes', () => {
   
-  // CT01
-  it('Selecionar o 1o checkbox da página', () => {
+  beforeEach(() => {
     cy.visit('https://the-internet.herokuapp.com/checkboxes') // acessa a página dos checkboxes
+  });
+
+  // CT01
+  it('Selecionar o 1o checkbox da página', () => {    
     cy.get("[type='checkbox']").first().check().should('be.checked') // clica no 1o input checkbox da página e verifica se está selecionado
   });
+
+  // CT02
+  it('Desmarcar o 2o checkbox da página', () => {
+    cy.get("[type='checkbox']").last().uncheck().should('not.be.checked') // desmarca o 2o input checkbox da página e verifica se não está selecionado
+  });
+
 });
