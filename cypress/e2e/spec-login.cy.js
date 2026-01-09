3445a06438e67928eed73d0058ebc577309ad9ae
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2026-01-09 14:34:26 -0300

    Finalização script CT01 spec Login

--- cypress/e2e/spec-login.cy.js
+++ cypress/e2e/spec-login.cy.js
@@ -9,5 +9,6 @@ describe('template spec', () => {
     cy.get("[name='username']").type("tomsmith") // preenche o campo username
     cy.get("[name='password']").type("SuperSecretPassword!") // preenche o campo password
     cy.get("button[type='submit']").click() // clica no botão de login
+    cy.get("h2").contains('Secure Area') // verifica se está na página correta após o login verificando o texto do título
   })
 })
