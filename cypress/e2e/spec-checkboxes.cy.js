4aab3320ea5077668a2317d112e8cd43c6c54b72
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2026-01-18 03:35:29 -0300

    Usando objetos de seletores no spec-checkboxes

--- cypress/e2e/spec-checkboxes.cy.js
+++ cypress/e2e/spec-checkboxes.cy.js
@@ -1,17 +1,22 @@
 describe('Página de checkboxes', () => {
   
+  // Objeto de seletores
+  const listaSeletores = {
+    campoCheckbox: "[type='checkbox']"
+  }
+
   beforeEach(() => {
     cy.visit('/checkboxes') // acessa a página dos checkboxes
   });
 
   // CT01
   it('Selecionar o 1o checkbox da página', () => {    
-    cy.get("[type='checkbox']").first().check().should('be.checked') // clica no 1o input checkbox da página e verifica se está selecionado
+    cy.get(listaSeletores.campoCheckbox).first().check().should('be.checked') // clica no 1o input checkbox da página e verifica se está selecionado
   });
 
   // CT02
-  it('Desmarcar o 2o checkbox da página', () => {
-    cy.get("[type='checkbox']").last().uncheck().should('not.be.checked') // desmarca o 2o input checkbox da página e verifica se não está selecionado
+  it.only('Desmarcar o 2o checkbox da página', () => {
+    cy.get(listaSeletores.campoCheckbox).last().uncheck().should('not.be.checked') // desmarca o 2o input checkbox da página e verifica se não está selecionado
   });
 
 });
